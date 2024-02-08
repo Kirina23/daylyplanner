93ec63097df25ce2980de8b4e68f8a31bd37272d
Irina Kirichenko <kira.airis23@gmail.com>
2024-02-08 12:00:57 +0600

    add user model

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+from django.db import models
+from django.utils import timezone
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, phone, password=None, **extra_fields):
+        # здесь должен быть код для создания пользователя
+        if not phone:
+            raise ValueError('The Phone field must be set')
+        user = self.model(phone=phone, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone, password=None, **extra_fields):
+        # Внимание: здесь нужен отступ перед кодом метода
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(phone, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    phone = models.CharField(max_length=17, unique=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.phone
--- backend/taskmanager/settings.py
+++ backend/taskmanager/settings.py
@@ -76,9 +76,14 @@ WSGI_APPLICATION = 'taskmanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'liss23',
+        'HOST': 'localhost',  # Обычно 'localhost' или '127.0.0.1' для локальной разработки
+        'PORT': '5432',  # Стандартный порт для PostgreSQL
     }
+
 }
 
 
@@ -122,3 +127,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'accounts.CustomUser'
