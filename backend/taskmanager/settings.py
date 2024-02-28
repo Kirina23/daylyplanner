9c609a906d33c51e3207591e7308eeeabbae47fc
Irina Kirichenko <kira.airis23@gmail.com>
2024-02-28 15:14:41 +0600

    add and fix

--- backend/taskmanager/settings.py
+++ backend/taskmanager/settings.py
@@ -1,6 +1,117 @@
 import os
+from pathlib import Path
 
+# Определение базового каталога проекта
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Секретный ключ для проекта
+SECRET_KEY = 'django-insecure-221lp135s3)fdk1qe(rc!ls=u303%)lm&s@p@8f0hzhv0x3_8g'
+
+# Включение режима отладки
+DEBUG = True
+
+# Список хостов/доменов, которым разрешено обслуживание данного проекта
+ALLOWED_HOSTS = []
+
+# Установленные приложения
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'accounts',
+    'rest_framework',
+    'corsheaders',
+]
+
+# Промежуточное программное обеспечение
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+# Конфигурация URL
+ROOT_URLCONF = 'taskmanager.urls'
+
+# Настройки шаблонов
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# Приложение WSGI
+WSGI_APPLICATION = 'taskmanager.wsgi.application'
+
+# Конфигурация базы данных
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'liss23',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }
+}
+
+# Валидаторы паролей
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Локализация
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+# Статические файлы
+STATIC_URL = 'static/'
+
+# Модель пользователей
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+# Фреймворк REST
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
+
+# Настройки CORS
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8081",
+]
 
 # Добавляем конфигурацию логирования
 LOGGING = {
@@ -28,111 +139,3 @@ LOGGING = {
         },
     },
 }
-
-
-
-
-from pathlib import Path
-
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-SECRET_KEY = 'django-insecure-221lp135s3)fdk1qe(rc!ls=u303%)lm&s@p@8f0hzhv0x3_8g'
-
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'accounts',
-    'rest_framework',
-    'corsheaders',
-]
-
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-     'corsheaders.middleware.CorsMiddleware',
-]
-
-ROOT_URLCONF = 'taskmanager.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'taskmanager.wsgi.application'
-
-DATABASES = {
-   'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': 'liss23',
-        'HOST': 'localhost',  # Обычно 'localhost' или '127.0.0.1' для локальной разработки
-        'PORT': '5432',  # Стандартный порт для PostgreSQL
-    }
-
-}
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
-
-LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
-
-USE_I18N = True
-
-USE_TZ = True
-
-STATIC_URL = 'static/'
-
-AUTH_USER_MODEL = 'accounts.CustomUser'
-
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-}
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:8081",
-]
-
--- backend/taskmanager/urls.py
+++ backend/taskmanager/urls.py
@@ -1,19 +1,10 @@
 from django.contrib import admin
-from django.urls import path, include
-from accounts.views import UserCreateView  # Убедитесь, что этот путь верен
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
-from accounts.views import UserCreateView, home 
+from django.urls import path
+from accounts.views import UserCreateView, home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Для упрощения, пути аутентификации сгруппированы в один include
-    path('api/auth/', include([
-        path('register/', UserCreateView.as_view(), name='register'),
-        path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-        path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    ])),
+    path('api/register/', UserCreateView.as_view(), name='register'),
     path('', home, name='home'),
+    # другие URL-адреса можно добавить здесь
 ]
